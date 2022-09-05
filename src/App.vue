89950c2a23bbb1cb448340bc017d8329e08156d5
Ste-cracco <s.cracco@yahoo.it>
2022-09-05 15:21:33 +0200

    Errore App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <MainHeader/>
+  <MainContent/>
+  <MainFooter/>
+</template>
+
+<script>
+import MainHeader from './components/MainHeader.vue'
+import MainContent from './components/MainContent.vue'
+import MainFooter from './components/MainFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainHeader,
+    MainContent,
+    MainFooter
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    ciao
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
